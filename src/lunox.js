edd47962345bb27c275595c21e39a37d2d077f73
plsletmeee <epic.game694@gmail.com>
2023-07-24 22:34:52 -0400

    Update lunox.js

--- src/lunox.js
+++ src/lunox.js
@@ -57,3 +57,16 @@ class MainClient extends Client {
 }
 
 module.exports = MainClient;
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
